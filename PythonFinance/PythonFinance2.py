49274789ee0449fbde8a8a0a92808ea57fc5aa3d
akhileshdixit <32309358+akhileshdixit@users.noreply.github.com>
2017-11-13 10:58:06 +0530

    Add files via upload

---
+++ PythonFinance/PythonFinance2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 20:31:23 2017
+
+@author: Akhilesh
+"""
+
+#import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+#import pandas_datareader.data as web
+
+style.use('ggplot')
+
+#start = dt.datetime(2000, 1, 1)
+#end = dt.datetime(2016, 12, 31)
+
+#dfw = web.DataReader('TSLA', 'yahoo', start, end)
+#dfw.to_csv('tsla.csv')
+
+dfr = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+
+#print(dfr.head())
+
+print(dfr[['Open', 'High']].head())
+
+dfr['Adj Close'].plot()
+plt.show()
